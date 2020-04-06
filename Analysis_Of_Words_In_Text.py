bad0c1c10abfa90acae094368dc3b658f171604d
bilgekrtgl <bilgekurtoglu06@gmail.com>
2020-04-06 20:56:46 +0300

    Find most repeated word pair in file

---
+++ Analysis_Of_Words_In_Text.py
@@ -0,0 +1,20 @@
+import nltk
+from nltk.collocations import *
+
+file_name = 'blake-poems.txt'
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+trigram_measures = nltk.collocations.TrigramAssocMeasures()
+
+def get_words(f):
+    text=f.read()
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    words_without_punctuation  = tokenizer.tokenize(text)
+    print(words_without_punctuation)
+    return words_without_punctuation
+
+words = get_words(open(file_name,'r'))
+
+finder = BigramCollocationFinder.from_words(words)
+word_pair = sorted(finder.nbest(trigram_measures.raw_freq, 1))
+
+print("The most repeated word pair in " +file_name+" : \" " +str (word_pair[0][0]) + " " + str(word_pair[0][1]) + " \"")
